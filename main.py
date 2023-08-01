744a2d2e64b14a4000074180caa04610548ccfb5
Kuba <j.pikula97@gmail.com>
2023-08-01 12:33:35 +0200

    generate_password() function created

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import tkinter
 from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    password = ''.join(password_list)
+
+    password_entry.insert(0, password)
+
+    pyperclip.copy(text=password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 def save():
 
@@ -15,7 +39,7 @@ def save():
     else:
 
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                          f"\nPassword: {password} \nIs it ok to save?")
+                                                              f"\nPassword: {password} \nIs it ok to save?")
 
         if is_ok:
             with open('data.txt', 'a') as data_file:
@@ -24,6 +48,7 @@ def save():
                 password_entry.delete(0, tkinter.END)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = tkinter.Tk()
 window.title('Password Manager')
@@ -54,7 +79,7 @@ password_label.grid(column=0, row=3)
 password_entry = tkinter.Entry(width=21)
 password_entry.grid(column=1, row=3)
 
-generate_password_button = tkinter.Button(text='Generate Password')
+generate_password_button = tkinter.Button(text='Generate Password', command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
 add_button = tkinter.Button(text='Add', width=36, command=save)
