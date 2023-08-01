815a98404725a934f9e6f64a858564ed67667354
Kuba <j.pikula97@gmail.com>
2023-08-01 10:08:18 +0200

    Window and canvas with image created

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import tkinter
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = tkinter.Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20, bg='white')
+
+canvas = tkinter.Canvas(width=200, height=200, bg='white', highlightthickness=0)
+logo_image = tkinter.PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo_image)
+canvas.pack()
+
+
+window.mainloop()
