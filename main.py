28ae656eb9c465449de7e0eca78418b84be5b2d6
Kuba <j.pikula97@gmail.com>
2023-08-01 12:12:51 +0200

    More functionalities to save() function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -9,10 +10,18 @@ def save():
     email = username_entry.get()
     password = password_entry.get()
 
-    with open('data.txt', 'a') as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, tkinter.END)
-        password_entry.delete(0, tkinter.END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title='Oops', message="Please make sure you haven't left any fields empty")
+    else:
+
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                          f"\nPassword: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open('data.txt', 'a') as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, tkinter.END)
+                password_entry.delete(0, tkinter.END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
